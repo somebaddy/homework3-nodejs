a7c38e940d496b5972c0757e33721e72fa31f161
Somebaddy <somebuddy.dev@gmail.com>
2025-05-11 15:01:32 +0300

    Add basic fighter service api

--- services/fighterService.js
+++ services/fighterService.js
@@ -2,6 +2,25 @@ import { fighterRepository } from "../repositories/fighterRepository.js";
 
 class FighterService {
   // TODO: Implement methods to work with fighters
+  getFighters() {
+    return fighterRepository.getAll();
+  }
+
+  getFighter(id) {
+    return fighterRepository.getOne({ id });
+  }
+
+  createFighter(fighter) {
+    return fighterRepository.create(fighter);
+  }
+
+  updateFighter(id, fighter) {
+    return fighterRepository.update(id, fighter);
+  }
+
+  deleteFighter(id) {
+    return fighterRepository.delete(id);
+  }
 }
 
 const fighterService = new FighterService();
