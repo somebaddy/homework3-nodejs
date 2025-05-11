50f8bd9cfda4480099bda9909ce830a31a7cb41d
Somebaddy <somebuddy.dev@gmail.com>
2025-05-11 23:16:16 +0300

    In response middleware add support error codes

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,17 +1,13 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
   if (res.err) {
-    if (!res.data) {
-      res.status(404).json({ 
-        error: true,
-        message: res.err.message
-      })
-    } else {
-      res.status(401).json({
-        error: true,
-        message: res.err.message
-      })
-    }
+    let { message, code } = res.err;
+    code = code || 500;
+    message = message || "Unknown error. That's embarrassing."
+    res.status(code).json({
+      error: true,
+      message
+    })
   } else {
     res.status(200).json(res.data)
   }
