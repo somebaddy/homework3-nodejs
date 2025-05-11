1bfa99fef97e8f230a9691f539e0be0c8c63a4ac
Somebaddy <somebuddy.dev@gmail.com>
2025-05-12 02:55:03 +0300

    Add fight logs validation

--- middlewares/fight.validation.middleware.js
+++ middlewares/fight.validation.middleware.js
@@ -1,6 +1,18 @@
+import { ValidationError } from "../helpers/errors.js";
 import { FIGHT } from "../models/fight.js";
 import { handleValidationError } from "./helpers.js";
 import { validateNoExtraFields, validateNoIdFieldInBody, validateRequiredFields } from "./validators.js";
+
+const validateLogs = (log) => {
+    if (!Array.isArray(log)) {
+        throw new ValidationError("Log must be an array");
+    }
+    log.forEach(item => {
+        const allowedFields = ["fighter1Shot", "fighter2Shot", "fighter1Health", "fighter2Health"];
+        validateRequiredFields(item, allowedFields);
+        validateNoExtraFields(item, allowedFields);
+    })
+}
 
 const createFightValid = (req, res, next) => {
     try {
@@ -11,6 +23,8 @@ const createFightValid = (req, res, next) => {
 
         const requiredFields = ["fighter1", "fighter2"];
         validateRequiredFields(req.body, requiredFields);
+
+        if (req.body.log) validateLogs(req.body.log);
 
         next();
     } catch (error) {
