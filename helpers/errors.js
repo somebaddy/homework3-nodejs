98ec7716688aa11434450fe3470401231152f881
Somebaddy <somebuddy.dev@gmail.com>
2025-05-11 23:17:15 +0300

    Add custom classes for some error codes

--- helpers/errors.js
+++ helpers/errors.js
@@ -7,7 +7,7 @@ class Error400 extends Error {
     constructor(message) {
         super(message);
         this.name = "BadRequest";
-        this.statusCode = 400;
+        this.code = 400;
     }
 }
 
@@ -15,7 +15,7 @@ class Error401 extends Error {
     constructor(message) {
         super(message);
         this.name = "Unauthorized";
-        this.statusCode = 401;
+        this.code = 401;
     }
 }
 
@@ -23,7 +23,7 @@ class Error404 extends Error {
     constructor(message) {
         super(message);
         this.name = "NotFound";
-        this.statusCode = 404;
+        this.code = 404;
     }
 }
 
@@ -31,12 +31,12 @@ class Error500 extends Error {
     constructor(message) {
         super(message);
         this.name = "InternalServerError";
-        this.statusCode = 500;
+        this.code = 500;
     }
 }
 
 
-module.exports = {
+export {
     Error400,
     Error401,
     Error404,
