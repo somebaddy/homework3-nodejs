8a9a814bb587e31c9c51c1de5e4b04c3a3a043dc
Somebaddy <somebuddy.dev@gmail.com>
2025-05-11 15:32:21 +0300

    Add basic api for fight service

--- services/fightService.js
+++ services/fightService.js
@@ -2,6 +2,17 @@ import { fightRepository } from "../repositories/fightRepository.js";
 
 class FightsService {
   // OPTIONAL TODO: Implement methods to work with fights
+  getFights() {
+    return fightRepository.getAll();
+  }
+
+  getFight(id) {
+    return fightRepository.getOne({ id });
+  }
+
+  createFight(fight) {
+    return fightRepository.create(fight);
+  }
 }
 
 const fightsService = new FightsService();
