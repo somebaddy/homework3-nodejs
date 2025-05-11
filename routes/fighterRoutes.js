92befc48af4084f2f07b5cb6de1c4d8dac36f925
Somebaddy <somebuddy.dev@gmail.com>
2025-05-11 14:56:02 +0300

    Add basic fighter routes

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -9,5 +9,64 @@ import {
 const router = Router();
 
 // TODO: Implement route controllers for fighter
+router.get("/", (req, res, next) => {
+  console.log("Fighters");
+  try {
+    const data = fighterService.getFighters();
+    res.data = data;
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.get("/:id", (req, res, next) => {
+  console.log("Fighter");
+  try {
+    const data = fighterService.getFighter(req.params.id);
+    res.data = data;
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.post("/", createFighterValid, (req, res, next) => {
+  console.log("Create Fighter");
+  try {
+    const data = fighterService.createFighter(req.body);
+    res.data = data;
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.patch("/:id", updateFighterValid, (req, res, next) => {
+  console.log("Update Fighter");
+  try {
+    const data = fighterService.updateFighter(req.params.id, req.body);
+    res.data = data;
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
+
+router.delete("/:id", (req, res, next) => {
+  console.log("Delete Fighter");
+  try {
+    const data = fighterService.deleteFighter(req.params.id);
+    res.data = data;
+  } catch (err) {
+    res.err = err;
+  } finally {
+    next();
+  }
+}, responseMiddleware);
 
 export { router };
